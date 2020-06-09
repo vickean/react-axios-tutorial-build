cd0acfd94e3178349211901ae3c1575688de7e83
vickean <vickean@gmail.com>
2020-06-09 15:42:46 +0800

    merging with master

---
+++ precache-manifest.dea4ffe8580e6176eabe0692ceb87ebe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e05fd5694daf8364b28464e27086094",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cd5b6ffc204827ed965",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "61f17cc3f79cde2be612",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3cd5b6ffc204827ed965",
+    "url": "/static/js/2.28602621.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.28602621.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61f17cc3f79cde2be612",
+    "url": "/static/js/main.ddd01492.chunk.js"
+  },
+  {
+    "revision": "441c092a927070357037",
+    "url": "/static/js/runtime-main.aa6d56ca.js"
+  }
+]);
